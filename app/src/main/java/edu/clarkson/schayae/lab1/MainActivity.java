b68f323c6dbdff974cf217cf58635e77000de86a
Alan Schay <alanschay@gmail.com>
2016-09-01 01:29:16 -0400

    Modified ListView object structure to use single view with two feeding arrays.

--- app/src/main/java/edu/clarkson/schayae/lab1/MainActivity.java
+++ app/src/main/java/edu/clarkson/schayae/lab1/MainActivity.java
@@ -2,9 +2,16 @@ package edu.clarkson.schayae.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> listA = new ArrayList<String>();
+    ArrayList<String> listB = new ArrayList<String>();
+    ArrayAdapter<String> adapterA;
+    ArrayAdapter<String> adapterB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
